48d616be63f7d871c6eb707e0cde54813739e09b
Pswrd <hoangducmanh123@gmail.com>
2023-10-17 17:15:51 +0700

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobDev21_Night_At_The_Museum"
+include(":app")
+ 
