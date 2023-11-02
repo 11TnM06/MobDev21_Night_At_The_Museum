b97b315a5b717dff31495a78c1c1c035769a82d3
anhson <sonanhnguyen003@gmail.com>
2023-11-02 10:38:14 +0700

    feat: login with email & password

---
+++ app/src/main/java/com/example/mobdev21_night_at_the_museum/presentation/intro/IntroductionActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobdev21_night_at_the_museum.presentation.intro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobdev21_night_at_the_museum.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
