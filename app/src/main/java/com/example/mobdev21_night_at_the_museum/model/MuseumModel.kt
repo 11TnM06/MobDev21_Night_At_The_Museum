4236883a6e453d17fcd646078e320a6c8d9e9c88
Pswrd <hoangducmanh123@gmail.com>
2023-11-30 05:50:35 +0700

    git add map api

--- app/src/main/java/com/example/mobdev21_night_at_the_museum/model/MuseumModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mobdev21_night_at_the_museum.domain.model
-
-import com.example.mobdev21_night_at_the_museum.IParcelable
-
-abstract class MuseumModel: IParcelable {
-    var key: String? = null
-}
--- app/src/main/java/com/example/mobdev21_night_at_the_museum/model/SimpleModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobdev21_night_at_the_museum.domain.model
-
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class SimpleModel: MuseumModel()
