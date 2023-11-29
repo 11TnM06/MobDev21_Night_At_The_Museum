592caf1db45e1df0b70603e7af889fd9835643ad
Pswrd <hoangducmanh123@gmail.com>
2023-11-30 06:10:24 +0700

    delete test model3D

--- app/src/main/java/com/example/mobdev21_night_at_the_museum/domain/repo/IFirebaseRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mobdev21_night_at_the_museum.domain.repo
-
-import java.io.File
-
-interface IFirebaseRepo {
-    fun get3DModel(name: String): File
-}
--- app/src/main/java/com/example/mobdev21_night_at_the_museum/domain/repo/IRemoteRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mobdev21_night_at_the_museum.domain.repo
-
-import com.example.mobdev21_night_at_the_museum.domain.model.MCollection
-import com.example.mobdev21_night_at_the_museum.domain.model.Model3d
-
-interface IRemoteRepo {
-    fun getListCollections(): List<MCollection>
-    fun getModelInCollection(collectionId: Int): List<Model3d>
-}
